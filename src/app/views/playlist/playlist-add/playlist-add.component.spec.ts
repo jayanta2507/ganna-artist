d3fb1c2a2a807be9b3bb52e7f6d7562b231daa52
vivek <vivek@gmail.com>
2021-06-20 13:18:47 +0530

    Myhardworking

---
+++ src/app/views/playlist/playlist-add/playlist-add.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PlaylistAddComponent } from './playlist-add.component';
+
+describe('PlaylistAddComponent', () => {
+  let component: PlaylistAddComponent;
+  let fixture: ComponentFixture<PlaylistAddComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PlaylistAddComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PlaylistAddComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
